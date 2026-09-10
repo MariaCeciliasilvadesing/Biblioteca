82acfa75a3fc5511c1043d335588c2b0d2ef9978
Sophia Dev <gustavo.tec2025@gmail.com>
2026-09-10 16:54:07 -0300

    Create biblioteca.py

---
+++ biblioteca.py
@@ -0,0 +1,26 @@
+while True:
+    print("=-=Menu da Biblioteca=-=")
+    print("0-Sair") 
+    print("1-cadastrar")
+    print("3-exibir")
+    print("4-Editar")
+    print("5-Pesquisar")
+    print("6-GerarRelatório")
+
+    op = int(input("Escolha uma opação do menu"))
+
+    if op == "0":
+
+    elif op == "1":
+
+    elif op == "2":
+
+    elif op == "3":
+
+    elif op == "4":
+
+    elif op == "5": 
+
+    elif op == "6":   
+
+    else:
