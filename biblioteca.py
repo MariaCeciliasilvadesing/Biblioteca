1d1d4431d8b9d72e282029f9c68a2bb462f677b7
Cecilianaiane <cecilia.naiane.tec2025@gmail.com>
2026-09-17 15:44:00 -0300

    criando menu

---
+++ biblioteca.py
@@ -0,0 +1,30 @@
+while True:
+    print("=-=Menu da Biblioteca=-=")
+    print("0-Sair") 
+    print("1-Cadastrar")
+    print("3-Exibir")
+    print("4-Deletar")
+    print("5-Editar")
+    print("6-Pesquisar")
+    print("7-Gerar relatório")
+
+    op = input("Escolha uma opação do menu")
+
+    if op == "0":
+        print("Aqui vai a opção de sair")
+
+    elif op == "1":
+
+    elif op == "2":
+
+    elif op == "3":
+
+    elif op == "4":
+
+    elif op == "5": 
+
+    elif op == "6": 
+
+    elif op == "7":   
+
+    else:
