5274ae4464672008e04576d065ba6ec0ae3356dd
Gustavo247 <gustavo.tec2025@gmail.com>
2026-09-17 23:02:23 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,17 +1,18 @@
 while True:
     print("=-=Menu da Biblioteca=-=")
     print("0-Sair") 
-    print("1-Cadastrar")
-    print("2-Exibir")
-    print("3-Editar")
-    print("4-Deletar")
+    print("1-cadastrar")
+    print("2-Deletar")
+    print("3-exibir")
+    print("4-Editar")
     print("5-Pesquisar")
-    print("6-Gerar Relatório")
+    print("6-GerarRelatório")
     
-    op = input("Escolha uma opação do menu")
+    op = input("Escolha uma opação do menu: ")
 
     if op == "0":
-        print("Aqui vai a opção de sair")
+        print("Saindo da Biblioteca...")
+        break
 
     elif op == "1":
 
